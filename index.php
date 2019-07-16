c9cf62a13a639360eb38b12121456c620569d6b8
Slavo <slv.sedlak@gmail.com>
2019-07-16 22:21:15 +0200

    index.php now create file for header

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <?php
 /**
+ *this script is for create part of code for arduino library for remote contorl for remote controll (tv-remote, led strip remote, magio remote, magio go remote)
  * Created by PhpStorm.
  * User: slavomir.sedlak
  * Date: 2019-07-16
  * Time: 18:21
  */
 run();
-
+/**
+ *structure of file remote-signal.txt
+ *first line is name of class
+ *then line start,name_of_method
+ *thne lines with signals for arduino
+ *after this line is stop line
+ *after stop line is start another method line
+ *and so on....
+ */
 function run(){
 $remoteSignalFile = fopen('remote-signal.txt', "r") or die('Unable to open file remote-signal.txt make sure if file exist!!!');
+$class = fgets($remoteSignalFile);
+file_put_contents('result-arduino-code-header.txt', "class $class{".PHP_EOL, FILE_APPEND | LOCK_EX)
+file_put_contents('result-arduino-code-header.txt', "public:".PHP_EOL, FILE_APPEND | LOCK_EX)
 while(!feof($remoteSignalFile)){
     $fileline = fgets($remoteSignalFile);
     $fileExplode  = explode(' ' , $fileline);
@@ -34,6 +46,7 @@ while(!feof($remoteSignalFile)){
             $number = explode(',',$number);
             $startFunction = "void $number[1]::$number[2](){";
             file_put_contents('result-arduino-code.txt', $startFunction.PHP_EOL, FILE_APPEND | LOCK_EX);
+	    file_put_contents('result-arduino-code-header.txt', "void $number[2]".PHP_EOL, FILE_APPEND | LOCK_EX)
             echo $startFunction . "<br>";
             continue;
         }
@@ -45,6 +58,8 @@ while(!feof($remoteSignalFile)){
 
     }
 }
+file_put_contents('result-arduino-code-header.txt', "private:".PHP_EOL, FILE_APPEND | LOCK_EX)
+file_put_contents('result-arduino-code-header.txt', "}".PHP_EOL, FILE_APPEND | LOCK_EX)
 fclose($remoteSignalFile);
 
 };
