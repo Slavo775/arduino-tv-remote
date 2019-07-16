2eb6ea65cc57408e1b64a9a2c044311dd00f37d5
Slavo <slv.sedlak@gmail.com>
2019-07-16 20:36:41 +0200

    Create arduino code for led strip remote

--- arduino-remote/arduino-remote.ino
+++ arduino-remote/arduino-remote.ino
@@ -1,14 +1,17 @@
 #include "arduino_remote_lib.h"
 
-remoteLedStrip ledStrip(13);
+remoteLedStrip ledStrip(D0);
 arduinoTvSamsungRemote samsungRemote;
 void setup() {
   Serial.begin(9600);
   ledStrip.OFFButton();
   samsungRemote.buttonOFF();
+  pinMode(D0,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  ledStrip.OFFButton();
+  delay(1000);
 
 }
