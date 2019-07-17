5851b5022d8c246743286cdf2ff4bc9eb5cfbe16
Slavo <slv.sedlak@gmail.com>
2019-07-17 20:09:22 +0200

    measure remote fo samsung tv

--- arduino-remote/arduino_tv_samsung_remote.cpp
+++ arduino-remote/arduino_tv_samsung_remote.cpp
@@ -1,5 +1,6 @@
 #include "arduino_tv_samsung_remote.h"
-
+#include "arduino_remote_lib.h"
 void arduinoTvSamsungRemote::buttonOFF(){
   Serial.println("Samsung remote OFF");
+  remoteLedStrip remote = new remoteLedStrip;
   }
